a5a1facf6d94a554aba2c185e01504166869d49a
WhiteBeltFullStack <128090204+WhiteBeltFullStack@users.noreply.github.com>
2024-01-03 12:14:27 +0200

    Add files via upload

--- project.js
+++ project.js
@@ -4,7 +4,7 @@ const MINE = '&#128163';
 const FLAG = '&#128681';
 
 var gMinesCount = 0;
-
+var gMinesRevealed = 0
 const gLevel = {
   SIZE: 4,
   MINES: 2,
@@ -17,15 +17,25 @@ var gGame = {
   secsPassed: 0,
 };
 
-
-var gBoard
+var gBoard;
 function onInit(size = gLevel.SIZE) {
-    gMinesCount = 0
-    gLevel.SIZE = size;
-    gLevel.MINES = Math.floor((size*size)*0.15)
-   gBoard = buildBoard(gLevel.SIZE);
-  console.log(gBoard);
+  gMinesRevealed = 0
+  var elRev = document.querySelector('.found')
+  elRev.innerText = 0
+  gMinesCount = 0;
+  gLevel.SIZE = size;
+  gLevel.MINES = Math.floor(size * size * 0.15);
+  gBoard = buildBoard(gLevel.SIZE);
+
+  // console.log(gBoard);
   renderBoard(gBoard);
+
+  minesOnBoard();
+  var elTdHideContent = document.querySelectorAll('td');
+  for (var i = 0; i < elTdHideContent.length; i++) {
+    elTdHideContent[i].innerText = '';
+  }
+  // console.log(gBoard);
 }
 
 function buildBoard(boardSize) {
@@ -41,66 +51,91 @@ function buildBoard(boardSize) {
       };
     }
   }
+
+  var firstClick = board;
   for (var i = 0; i < gLevel.MINES; i++) {
     var minePlaced = false;
     while (!minePlaced) {
       var randomRow = getRandomInt(0, boardSize - 1);
       var randomCol = getRandomInt(0, boardSize - 1);
-
-      if (!board[randomRow][randomCol].isMine) {
-        board[randomRow][randomCol].isMine = true;
-        minePlaced = true;
+      if (board[randomRow][randomCol] !== firstClick) {
+        if (!board[randomRow][randomCol].isMine) {
+          board[randomRow][randomCol].isMine = true;
+          minePlaced = true;
+          gMinesCount++;
+        }
       }
     }
   }
-    for(var i = 0 ; i < boardSize; i++){
-        for(var j = 0 ; j < boardSize;j++){
-            setMinesNegsCount(board,i,j)
-        }
+  for (var i = 0; i < boardSize; i++) {
+    for (var j = 0; j < boardSize; j++) {
+      setMinesNegsCount(board, i, j);
     }
+  }
   return board;
 }
 
-function setMinesNegsCount(board,rowIdx,colIdx) {
-    for(var i = rowIdx-1 ; i<= rowIdx+1;i++ ){
-        if(i<0 || i >= board.length) continue
-        for(var j = colIdx -1 ;j <= colIdx+1 ;j++){
-            if(j < 0 || j >= board[i].length) continue
-            if(i === rowIdx && j === colIdx) continue
-            if(board[i][j].isMine){
-                board[rowIdx][colIdx].minesAroundCount++
-                
-            } 
-        }
+function setMinesNegsCount(board, rowIdx, colIdx) {
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i >= board.length) continue;
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j >= board[i].length) continue;
+      if (i === rowIdx && j === colIdx) continue;
+      if (board[i][j].isMine) {
+        board[rowIdx][colIdx].minesAroundCount++;
+      }
     }
+  }
+}
+
+function onCellClicked(elCell, i, j, event) {
+  // console.log('Clicked:', event);
+
+  elCell.style.backgroundColor = 'darkslategray';
+
+ 
+
+  if (gBoard[i][j].isMine && !gBoard[i][j].isShown) {
+    elCell.innerHTML = MINE;
+    gBoard[i][j].isShown = true;
+    gMinesRevealed++
+    var elRev = document.querySelector('.found')
+    elRev.innerText = gMinesRevealed
     
   }
 
-// function setMinesNegsCount(board,rowidx,colIdx) {
-//   for (var i = 0; i < board.length; i++) {
-//     for (var j = 0; j < board[i].length; j++) {
-//         if(board[i][j].isMine){
-//             gMinesCount++
-            
-//         }
-//     }
-//   }
-//   return gMinesCount
-// }
-
-function onCellClicked(elCell, i, j) {
-  console.log(elCell, i, j);
+  if (!gBoard[i][j].isMine && !gBoard[i][j].isShown) {
+    elCell.innerText = gBoard[i][j].minesAroundCount;
+    gBoard[i][j].isShown = true;
+    if (gBoard[i][j].minesAroundCount === 0) {
+      elCell.innerText = ' ';
+    }
+  }
+  if (event.button === 2){
+    onCellMarked(elCell, event, i, j);
+  }
 }
 
-function onCellMarked(elCell) {}
+function onCellMarked(elCell, ev, i, j) {
+  ev.preventDefault();
+  console.log(ev);
+  
+    if (gBoard[i][j].isShown) {
+      return;
+    }
+    if (!gBoard[i][j].isMarked) {
+      elCell.innerText = 'FLAG';
+    } else {
+      elCell.innerText = '';
+    }
+  
+}
 
 function checkGameOver() {}
 
 function expandSHown(board, elCell, i, j) {}
 
-
-
-function minesOnBoard(){
-    var elOnBoard = document.querySelector('.on-board')
-    elOnBoard.innerText = gMinesCount
+function minesOnBoard() {
+  var elOnBoard = document.querySelector('.on-board');
+  elOnBoard.innerText = gMinesCount;
 }
--- util.js
+++ util.js
@@ -1,36 +1,27 @@
-'use strict'
+'use strict';
 
 function renderBoard(board) {
-    var strHTML = `<table><tbody>\n`
-    for (var i = 0; i < board.length; i++) {
-        strHTML += `<tr>\n`
-        for (var j = 0; j < board[i].length; j++) {
-            var cellClassName = `cell cell-${i}-${j}`
-            var cellInner = board[i][j].isMine ? MINE : board[i][j].minesAroundCount;
-           
-            strHTML += `<td onclick="onCellClicked(this,${i},${j})" class="${cellClassName}">${cellInner}</td>\n`
-        }
-        strHTML += `</tr>\n`
-    }
-    strHTML += `</table></tbody>\n`
+  var strHTML = `<table ><tbody>\n`;
+  for (var i = 0; i < board.length; i++) {
+    strHTML += `<tr>\n`;
+    for (var j = 0; j < board[i].length; j++) {
+      var cellClassName = `cell cell-${i}-${j}`;
+      var cellInner = board[i][j].isMine ? MINE : board[i][j].minesAroundCount;
 
-    
-    // console.log(strHTML);
-    const elGameContainer = document.querySelector('.game-container')
-    elGameContainer.innerHTML = strHTML
+      strHTML += `<td  onclick="onCellClicked(this,${i},${j},event)" 
+             class=" ${cellClassName}">${cellInner}</td>\n`;
+    }
+    strHTML += `</tr>\n`;
+  }
+  strHTML += `</table></tbody>\n`;
+  
+  
+  const elGameContainer = document.querySelector('.game-container');
+  elGameContainer.innerHTML = strHTML;
 }
 
-
-
-
-
-
-
-
-
-
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
